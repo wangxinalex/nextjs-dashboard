db7566e34d78a906264bbc06434829d671ef8e1f
Xin Wang <wangxinalex@gmail.com>
2024-10-31 21:45:52 +0800

    fix: fix lint errors

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -14,6 +14,7 @@ export function CreateInvoice() {
 }
 
 export function UpdateInvoice({ id }: { id: string }) {
+  console.log(id);
   return (
     <Link
       href="/dashboard/invoices"
@@ -25,6 +26,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  console.log(id);
   return (
     <>
       <button className="rounded-md border p-2 hover:bg-gray-100">
